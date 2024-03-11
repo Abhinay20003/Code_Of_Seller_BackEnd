be676df42fd981ccd15c8c8fae822761652d98e9
Abhinay20003 <18891a05e8.abhinaykumar@gmail.com>
2024-03-11 12:54:16 +0530

    Differentiated reg and log dto

--- SellerRegistration-service/src/main/java/com/SellerRegistrationservice/controller/SellerRegistrationController.java
+++ SellerRegistration-service/src/main/java/com/SellerRegistrationservice/controller/SellerRegistrationController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.SellerRegistrationservice.dto.LoginDTO;
 import com.SellerRegistrationservice.dto.SellerRegistrationDTO;
 import com.SellerRegistrationservice.service.SellerRegistrationService;
 
@@ -41,16 +41,17 @@ public class SellerRegistrationController {
 	}
 
 	@PostMapping("/login")
-	public ResponseEntity<String> login(@RequestParam String emailID, @RequestParam String password) {
-		if (emailID.isEmpty() || password.isEmpty()) {
+	public ResponseEntity<String> login(@RequestBody LoginDTO loginRequest) {
+		if (loginRequest == null || loginRequest.getEmailID().isEmpty() || loginRequest.getPassword().isEmpty()) {
 			return new ResponseEntity<>("EmailID or Password can't be empty", HttpStatus.BAD_REQUEST);
 		}
 
-		boolean isValidLogin = sellerRegistrationService.validateLogin(emailID, password);
+		boolean isValidLogin = sellerRegistrationService.validateLogin(loginRequest);
 		if (isValidLogin) {
 			return new ResponseEntity<>("Login successful", HttpStatus.OK);
 		} else {
 			return new ResponseEntity<>("Invalid email or password", HttpStatus.UNAUTHORIZED);
 		}
 	}
+
 }
